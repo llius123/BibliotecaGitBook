5453b3a718ff4836675cc983c81ba20603267130
Jesus Berisa Nuñez <33457979+llius123@users.noreply.github.com>
2020-03-06 20:21:14 +0100

    Fecha: 06/03/2020 , hora: 20:21:13,55

--- script.bat
+++ script.bat
@@ -1,14 +1,21 @@
-@echo off
+echo Construir el libro
+echo ----------------------
+
+mdbook build
 
 set fecha=%date%
 set hora=%time%
 
 set commit=Fecha: %fecha% , hora: %hora%
 
-
+echo Añadido archivos modificados
+echo ----------------------
 git add -A --
 
+echo Añadido mensaje al commit
+echo ----------------------
 git commit -m "Fecha: %fecha% , hora: %hora%"
 
+echo Push al github/master
 git push github master
 pause
